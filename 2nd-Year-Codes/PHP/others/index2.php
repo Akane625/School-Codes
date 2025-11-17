1b0afd601a686e3581a029bb519deb74fefef1a2
Akane625 <yutucthomdaniel3@gmail.com>
2025-11-17 10:04:49 +0800

    Create index2.php

---
+++ 2nd-Year-Codes/PHP/others/index2.php
@@ -0,0 +1,17 @@
+<title>Thom Yutuc PHP</title>
+
+<h1>Thom Yutuc PHP</h1>
+
+<pre>
+    *****************
+            *
+            *
+            *
+            *
+            *
+            *
+</pre>
+
+<?php
+echo "The SHA256 of 'Thom Yutuc' is"; print hash('sha256', 'Thom Yutuc');
+?>
